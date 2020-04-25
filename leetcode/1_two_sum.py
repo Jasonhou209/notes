ac380d3deea8bc9ebbc0b4b4aa3e4fe0dbfc22a2
Jasonhou209 <564481431@qq.com>
2020-04-25 18:46:07 +0800

    format

--- leetcode/1_two_sum.py
+++ leetcode/1_two_sum.py
@@ -1,20 +1,19 @@
 """
+1.给定一个整数数组nums和一个目标值target,请你在该数组中找出和为目标值的那两个整数,
+并返回他们的数组下标。你可以假设每种输入只会对应一个答案。
+但是,你不能重复利用这个数组中同样的元素。
 
-    1.给定一个整数数组nums和一个目标值target,请你在该数组中找出和为目标值的那两个整数,并返回他们的数组下标。
-      你可以假设每种输入只会对应一个答案。但是,你不能重复利用这个数组中同样的元素。
-
-    示例:
-        给定 nums =[2, 7, 11, 15], target =9
-        因为 nums[0] +nums[1] =2 +7 =9
-        所以返回 [0, 1]
-
+示例:
+给定 nums =[2, 7, 11, 15], target =9
+因为 nums[0] +nums[1] =2 +7 =9
+所以返回 [0, 1]
 """
 
 class Solution:
 
     def __init__(self):
         self.result = []
-    
+
     def twoSum(self, nums: list, target: int) -> list:
         for i in range(len(nums) - 1):
             for j in range(i+1, len(nums)):
@@ -23,11 +22,13 @@ class Solution:
                     self.result.append(j)
         return self.result
 
-if __name__ == "__main__":
-
+def main():
     nums = [2, 7, 11, 15]
     target = 9
 
     s = Solution()
     r = s.twoSum(nums, target)
     print(r)
+
+if __name__ == "__main__":
+    main()
--- leetcode/20_valid_parentheses.py
+++ leetcode/20_valid_parentheses.py
@@ -1,38 +1,30 @@
 """
-	给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+20.给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
 
-	有效字符串需满足：
+有效字符串需满足：
+1. 左括号必须用相同类型的右括号闭合。
+2. 左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
 
-	1. 左括号必须用相同类型的右括号闭合。
-	2. 左括号必须以正确的顺序闭合。
-	
-	注意空字符串可被认为是有效字符串。
+示例 1:
+输入: "()"
+输出: true
 
-	示例 1:
+示例 2:
+输入: "()[]{}"
+输出: true
 
-		输入: "()"
-		输出: true
-	
-	示例 2:
+示例 3:
+输入: "(]"
+输出: false
 
-		输入: "()[]{}"
-		输出: true
-	
-	示例 3:
+示例 4:
+输入: "([)]"
+输出: false
 
-		输入: "(]"
-		输出: false
-	
-	示例 4:
-
-		输入: "([)]"
-		输出: false
-	
-	示例 5:
-
-		输入: "{[]}"
-		输出: true
-
+示例 5:
+输入: "{[]}"
+输出: true
 """
 
 class Solution:
@@ -43,25 +35,26 @@ class Solution:
 
     def isValid(self, s: str) -> bool:
 
-    	for v in s:
-    		if v in self.d:
-    			self.stack.append(v)
-    		elif len(self.stack) == 0:
-    			return False
-    		elif self.d[self.stack.pop()] == v:
-    			continue
-    		else:
-    			return False
-    	else:
-    		return True
+        for v in s:
+            if v in self.d:
+                self.stack.append(v)
+            elif len(self.stack) == 0:
+                return False
+            elif self.d[self.stack.pop()] == v:
+                continue
+            else:
+                return False
+        else:
+            return True
 
+def main():
+    s = Solution()
+    print("()", "=>", s.isValid("()"))
+    print("()[]{}", "=>", s.isValid("()[]{}"))
+    print("(]", "=>", s.isValid("(]"))
+    print("([)]", "=>", s.isValid("([)]"))
+    print("{[]}", "=>", s.isValid("{[]}"))
+    print("()[{]}", "=>", s.isValid("()[{]}"))
 
 if __name__ == "__main__":
-
-	s = Solution()
-	print("()", "=>", s.isValid("()"))
-	print("()[]{}", "=>", s.isValid("()[]{}"))
-	print("(]", "=>", s.isValid("(]"))
-	print("([)]", "=>", s.isValid("([)]"))
-	print("{[]}", "=>", s.isValid("{[]}"))
-	print("()[{]}", "=>", s.isValid("()[{]}"))
+    main()
