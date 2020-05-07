364d27370428e14269147763998d454e0cdcae8a
Jasonhou209 <564481431@qq.com>
2020-05-07 20:13:46 +0800

    add reverse linked list

---
+++ leetcode/206_reverse_linked_list.py
@@ -0,0 +1,39 @@
+"""
+206.反转链表
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        cur = head
+        while cur != None:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
+
+def main():
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+
+    resultNode = Solution().reverseList(head)
+    while resultNode != None:
+        print(resultNode.val, end=" ")
+        resultNode = resultNode.next
+
+if __name__ == "__main__":
+    main()
