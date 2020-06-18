84d4de27125f5950ceca9354822f09f6c8dc1a16
Jasonhou209 <564481431@qq.com>
2020-06-18 20:35:10 +0800

    add validate binary search tree

---
+++ leetcode/98_validate_binary_search_tree.py
@@ -0,0 +1,57 @@
+"""
+98.验证二叉搜索树
+给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+假设一个二叉搜索树具有如下特征：
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+示例 1:
+输入:
+    2
+   / \
+  1   3
+输出: true
+示例 2:
+
+输入:
+    5
+   / \
+  1   4
+     / \
+    3   6
+输出: false
+解释: 输入为: [5,1,4,null,null,3,6]。
+     根节点的值为 5 ，但是其右子节点值为 4 。
+"""
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        inorder = []
+        def helper(root):
+            if root:
+                helper(root.left)
+                inorder.append(root.val)
+                helper(root.right)
+        helper(root)
+        for i in range(len(inorder)-1):
+            if inorder[i+1] <= inorder[i]:
+                return False
+        return True
+
+def main():
+    root = TreeNode(5)
+    root.left = TreeNode(1)
+    root.right = TreeNode(4)
+    root.right.left = TreeNode(3)
+    root.right.right = TreeNode(6)
+
+    s = Solution()
+    print(s.isValidBST(root))
+
+if __name__ == "__main__":
+    main()
